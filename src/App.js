64a398939a15aef0bdd60b05e72ce3701613d03d
spriss <spriss@uw.edu>
2022-02-18 21:00:12 -0800

    location input for weather api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Weather App</h1>
+        <h1>Sunset Spotter</h1>
       </header>
         <main>
           <Forecast />
--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import {
+    Wrapper,
+    Small,
+    Loader
+} from './Conditions.css'
 
-const Conditions = (props) => {
-   return (
-       <div>
-           {props.responseObj.cod === 200 ?
-               <div>
-                   <p><strong>{props.responseObj.name}</strong></p>
-                   <p>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
-               </div>
-           : null
-           }
-       </div>
-   )
+const conditions = (props) => {
+    return (
+        <div className="Wrapper">
+
+            {props.error && <small className="Small">Please enter a valid city.</small>}
+
+            {props.loading && <div className="Loader" />}
+
+
+            {props.responseObj.cod === 200 ?
+                <div>
+                    <p><strong>{props.responseObj.name}</strong></p>
+                    <p>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
+                </div>
+            : null
+            }
+        </div>
+    )
 }
 
-export default Conditions;
+export default conditions;
