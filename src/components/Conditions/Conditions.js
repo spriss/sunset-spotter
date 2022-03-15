d591d52faf19c344fe85c15d64f2e6d9ec8c4f9a
spriss <spriss@uw.edu>
2022-03-15 14:35:34 -0700

    sunset time added

--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -5,8 +5,21 @@ import {
     Loader
 } from './Conditions.css'
 
+function sunsetTime(time) {
+
+    let unix_timestamp = (time)
+    var date = new Date(unix_timestamp * 1000);
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var formattedTime = hours + ':' + minutes.substr(-2);
+    return formattedTime;
+
+}
+
 const conditions = (props) => {
+    
     return (
+
         <div className="Wrapper">
 
             {props.error && <small className="Small">Please enter a valid city.</small>}
@@ -16,7 +29,7 @@ const conditions = (props) => {
 
             {props.responseObj.cod === 200 ?
                 <div>
-                    <p className='response'>In {props.responseObj.name} it is currently {Math.round(props.responseObj.main.temp)} degrees with {props.responseObj.weather[0].description}. </p>
+                    <p className='response'>In {props.responseObj.name} it is currently {Math.round(props.responseObj.main.temp)} degrees with {props.responseObj.weather[0].description}. Sunset today is at: {sunsetTime(props.responseObj.sys.sunset)} PT.  </p>
                 </div>
             : null
             }
